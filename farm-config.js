4d72259cd1bb244a32c8638ecbb12a51cd75794a
Ransege <122806717+Ransege@users.noreply.github.com>
2025-12-19 15:20:18 +0300

    Delete farm-config.js

--- farm-config.js
+++
@@ -1,8 +0,0 @@
-const BASE_LIMIT = 5000;
-const limitIncreases = [0, 2000, 3500, 5000, 7000, 10000];
-const limitCosts = [0, 2000, 5000, 7500, 12000, 25000];
-
-const hackCosts = [0, 2000, 4500, 8000, 14000, 20000, 35000, 50000, 75000, 100000];
-
-const minerRates = [0, 300, 900, 1800, 6000, 18000];
-const minerCosts = [0, 500, 2000, 3500, 10000, 35000];
